3c1d8614fb63357c457658dda0bcfc5abd930be9
OceanNuclear <annoyingamygdala@gmail.com>
2021-05-15 04:09:54 +0100

    first commit

---
+++ Poisson_sqrt.py
@@ -0,0 +1,62 @@
+from numpy import array as ary; from numpy import log as ln
+from numpy import cos, sin, pi, sqrt, exp, arccos;
+tau = 2*pi
+import numpy as np;
+from matplotlib import pyplot as plt
+# from math import factorial
+# fac = lambda a: ary([factorial(i) for i in a])
+from scipy.special import factorial as fac
+
+def mean_variance(distribution_x, distribution_P):
+    mean = (distribution_x * distribution_P).sum() # scalar
+    variance = ((distribution_x - mean)**2 * distribution_P).sum() # method 1
+    # variance = (distribution_x**2 * P_N).sum() - mean**2 # method 2
+    return mean, variance # two scalars
+
+# kernel = [0.125, 0.375, 0.375, 0.125] # this gives a factor of exactly 3.2 reduction from the variance of y to the variance of convolved distribution
+# kernel = [0.25, 0.25, 0.25, 0.25] # this gives a factor of exactly 3.2 reduction from the variance of y to the variance of convolved distribution
+# kernel = [0.00, 0.00, 0.50, 0.50] # this gives a factor of exactly 3.2 reduction from the variance of y to the variance of convolved distribution
+kernel = [0.00, 0.00, 0.60, 0.40] # this gives a factor of exactly 3.2 reduction from the variance of y to the variance of convolved distribution
+if __name__=='__main__':
+    # lambda: the free variable to change.
+    N = np.arange(100) # range of all natural numbers. 1000 is close enough to infinity.
+    y = sqrt(N) # sqrt(counts) representation
+    y_line, c_line, y_distortion, sqrt_line = [], [], [], []
+
+    # for lamb in np.logspace(-1, 2, 30):
+    for lamb in np.logspace(-1, np.log10(20)):
+        print("processing lambda=", lamb)
+        gaussian_mean = N_mean = gaussian_var = N_var = lamb
+        
+        # lamb = 20.0 # 1.2 # 2.0 # 0.5
+        P_N_precursor = fac(N) * lamb**-N
+        P_N = exp(-lamb) * np.nan_to_num(1/P_N_precursor, nan=0.0, posinf=0.0, neginf=0.0) # the pmf: probability mass function
+
+        y_mean, y_var = mean_variance(y, P_N)
+
+        c_dist_x, c_dist_P = ary([0.0]), ary([1.0]) #convolved distribution 
+        for k in kernel:
+            c_dist_x = np.add.outer(c_dist_x, k*y).flatten()
+            c_dist_P = np.outer(c_dist_P, P_N).flatten()
+        print("Captured {} of the full probability distribution".format(c_dist_P.sum()))
+
+        # convolved distribution's parameters: mean, variance, etc.
+        c_mean, c_var = mean_variance(c_dist_x, c_dist_P)
+        y_line.append((y_mean, y_var))
+        c_line.append((c_mean, c_var))
+
+        y_distortion.append((N_mean, c_mean))
+        sqrt_line.append((N_mean, sqrt(N_mean)))
+        print()
+    plt.plot(*ary(y_distortion).T, label="actual mean")
+    plt.plot(*ary(sqrt_line).T, label="sqrt approx. mean")
+    plt.title("sqrt distribution mean's deviation\nfrom the sqrt mean(N) approximation")
+    plt.show()
+    plt.plot(*ary(y_line).T)
+    plt.plot(*ary(c_line).T)
+    plt.title("after pplying c_lin")
+    plt.show()
+"""
+Conclusion:
+y_var started at 0.3783934365408028 at lamb=0.1, and asymptotically approaches around 0.25
+"""
---
+++ sqrt_repr.py
@@ -0,0 +1,64 @@
+from numpy import array as ary; from numpy import log as ln
+from numpy import cos, sin, pi, sqrt, exp, arccos;
+tau = 2*pi
+import sys
+import numpy as np;
+import pandas as pd
+from matplotlib import pyplot as plt
+
+"""This is the question I want to be asking: why aren't we using the sqrt y style of plotting???"""
+
+def plot_sqrt(*args, ax=None, rewrite_y_axis_label=True):
+    """
+    plot an n bin gamma spectrum in sqrt representation.
+    Parameters
+    ----------
+    [optional] boundary_2d : numpy array of shape (n, 2) denoting the bin edges (upper edge, lower edge)
+    counts : numpy array of shape (n) denoting the means
+    ax: Axes on which to plot the graph if one already exists. Otherwise a new Axes will be created.
+    rewrite_y_axis_label: Whether to (True) rewrite the ylabels so they reflect counts;
+                            or to (False) keep the y label as sqrt(counts).
+                            Note that this option comes with the caveat of needing extra flux space.
+    """
+    assert len(args)<=2, "Call signature = plot_sqrt([boundary_2d], counts, ax=None, rewrite_y_axis_label=False)".format()
+    if ax is None:
+        ax = plt.subplot()
+
+    has_x = len(args)==2
+    if has_x:
+        boundary_2d, counts = args
+        ax.set_xlabel("gamma E (keV)")
+    else:
+        counts = args[0]
+        boundary_2d = ary([np.arange(0, len(counts)), np.arange(1,len(counts)+1)]).T
+        ax.set_xlabel("bins")
+    transformed_cnts = sqrt(counts)
+    plot, = ax.plot(boundary_2d.flatten(), np.repeat(transformed_cnts, 2))
+    ax.set_ylabel("sqrt (counts)")
+    if rewrite_y_axis_label:
+        ax.set_ylabel("counts")
+        ax.set_yticklabels(np.sign(yticks:=ax.get_yticks()) * yticks**2)
+    ax.set_title("sqrt-y plot of gamma spec")
+    return ax, plot
+
+def plot_log(*args, ax=None):
+    """
+    plot an n-bin gamma spectrum in log-log representation
+    ax: Axes on which to plot the graph if one already exists. Otherwise a new Axes will be created.
+    """
+    if ax is None:
+        ax = plt.subplot()
+    plot, = ax.semilogy(boundary_2d.flatten(), np.repeat(counts, 2))
+    ax.set_xlabel("gamma E (keV)")
+    ax.set_ylabel("counts")
+    ax.set_title("log plot of gamma spec")
+    return ax, plot
+
+if __name__=='__main__':
+    spectrum = pd.read_csv(sys.argv[1], index_col=[0])
+    boundaries = spectrum[["lenergy", "uenergy"]].to_numpy()
+    counts = spectrum["count"].to_numpy()
+    fig, (axtop, axbot) = plt.subplots(2, 1, sharex=True)
+    plot_sqrt(boundaries, counts, ax=axtop, rewrite_y_axis_label=False)
+    plot_log(boundaries, counts, ax=axbot)
+    plt.show()
