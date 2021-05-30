ea36d8eb76588bd9d41c0a395efe69e690b23df9
OceanNuclear <annoyingamygdala@gmail.com>
2021-05-30 19:25:31 +0100

    Optimized the default_peak_identifier's default parameters (curvature_threshold, peakiness_threshold, peakiness_function) using the example_file/background.Spe. Further optimization could be done (Using a machine learning framework) but would be beyond the scope of today's investigation.

--- decision_explanation.py
+++ decision_explanation.py
@@ -19,27 +19,36 @@ import inspect
 if DEMONSTRATE_DECISION_MAKING:
     # calculate the required values first.
     curvature_coef = spectrum.calculate_sqrt_curvature()
-    peakiness = spectrum.apply_function_on_peakiness(np.nanmean)
-    peak_indices = spectrum.default_peak_identifier()
-    curvature_threshold, peakiness_threshold = inspect.signature(spectrum.default_peak_identifier).parameters.values()
-
+    mean_peakiness = spectrum.apply_function_on_peakiness(np.nanmean)
+    max_peakiness = spectrum.apply_function_on_peakiness(np.nanmax)
+    peakiness = spectrum.get_peakiness()
+    curvature_threshold, peakiness_threshold, _ = inspect.signature(spectrum.default_peak_identifier).parameters.values()
 
     # actual spectrum (ax_u)
     fig, (ax_u, ax_m, ax_l) = plt.subplots(3, 1, sharex=True)
     spectrum.plot_sqrt_scale(ax=ax_u)
-    spectrum.highlight_peaks(ax_u, peak_indices)
+    spectrum.highlight_peaks(ax_u, spectrum.default_peak_identifier(), alpha=0.5)
+    spectrum.highlight_peaks(ax_u, spectrum.faster_peak_identifier(), color="C2", alpha=0.5)
+    spectrum.highlight_peaks(ax_u, spectrum.default_peak_identifier(curvature_threshold=-2.1, peakiness_function=np.nanmax), color="C3", alpha=0.5)
 
     # curvature (ax_m)
-    ax_m.plot(spectrum.boundaries.flatten(), np.repeat(curvature_coef, 2))
     threshold_func = threshold_curve_function_generator(curvature_threshold.default, *spectrum.fwhm_cal)
-    ax_m.plot(spectrum.boundaries.flatten(), threshold_func(spectrum.boundaries.flatten()) )
+    ax_m.plot(spectrum.boundaries.flatten(), threshold_func(spectrum.boundaries.flatten()), label="threshold 1")
+    ax_m.plot(spectrum.boundaries.flatten(), np.repeat(curvature_coef, 2))
+    threshold_func = threshold_curve_function_generator(-2.1, *spectrum.fwhm_cal)
+    ax_m.plot(spectrum.boundaries.flatten(), threshold_func(spectrum.boundaries.flatten()), label="threshold 2", color="C3")
+    ax_m.legend()
     ax_m.set_ylim(-5, -0.1)
     ax_m.set_title("Curvatrue")
 
     # peakiness (ax_l)
-    ax_l.plot(spectrum.boundaries.flatten(), np.repeat(peakiness, 2))
-    ax_l.plot(spectrum.boundaries.flatten(), np.repeat(peakiness_threshold.default, len(spectrum.boundaries.flatten())))
+    ax_l.plot(spectrum.boundaries.flatten(), np.repeat(peakiness_threshold.default, len(spectrum.boundaries.flatten())), label="threshold")
+    ax_l.plot(spectrum.boundaries.flatten(), np.repeat(mean_peakiness, 2), label="self-peakiness mean values")
+    ax_l.plot(spectrum.boundaries.flatten(), np.repeat(peakiness, 2), label="true peakiness values")
+    ax_l.plot(spectrum.boundaries.flatten(), np.repeat(max_peakiness, 2), label="self-peakiness max values")
+    ax_l.plot(spectrum.boundaries.flatten(), np.repeat(0.6, len(spectrum.boundaries.flatten())), label="threshold for max")
     ax_l.set_title("Peakiness")
+    ax_l.legend()
 
     plt.show()
 
@@ -47,5 +56,4 @@ else:
     ax, line = spectrum.plot_sqrt_scale()
     peak_indices = spectrum.default_peak_identifier()
     spectrum.highlight_peaks(ax, peak_indices)
-
     plt.show()
